ffb4ad218b3ef7be93af8cb4f95345992a3cb1f7
Juliana  Faustino de Freitas Duarte <1420539@ises.com.br>
2018-04-05 08:41:10 -0300

    implementando soma de pontos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("script carregado");
+
+var pts = parseInt(localStorage.getItem("pontos"));
+
+function confere() {
+	if (document.getElementById("certo").checked) {
+		pts += 10;
+		localStorage.setItem("pontos", pts);
+	}
+}
+
+console.log(localStorage.getItem("pontos"));
