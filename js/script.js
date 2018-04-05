e39227f96e32b27466ff7186dac49ee551ddc863
Juliana Faustino de Freitas Duarte <cin_jduarte@uolinc.com>
2018-04-05 10:53:09 -0300

    funcionalidade total pontos

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 
 var pts = parseInt(localStorage.getItem("pontos"));
 
-function confere() {
+function confere(jogo) {
 	if (document.getElementById("certo").checked) {
 		pts += 10;
 		localStorage.setItem("pontos", pts);
 	}
+
+	if (jogo) {
+		ga('send', 'event', jogo, 'pontos', pts);
+	}
 }
 
 console.log(localStorage.getItem("pontos"));
+
+function verResultado () {
+	document.getElementById("resultado").innerHTML = localStorage.getItem("pontos");
+}
